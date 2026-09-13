c03f5e3859c6222755d4da9dd71a0414518e8923
Mohammed alqallaf <elqallaf09@gmail.com>
2026-09-14 00:32:32 +0300

    feat(economic-intelligence): export macro context contracts

--- src/domain/economic-intelligence/index.ts
+++ src/domain/economic-intelligence/index.ts
@@ -2,4 +2,5 @@ export * from './types';
 export * from './digitalTwin';
 export * from './decisionEngine';
 export * from './dataSource';
+export * from './economicContext';
 export * from './version';
