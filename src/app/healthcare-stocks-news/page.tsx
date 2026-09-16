52486a4bb534ad0e6deba1d39d45f83dc5f54d87
Mohammed alqallaf <elqallaf09@gmail.com>
2026-09-16 22:17:57 +0300

    feat(news): add healthcare stocks news page

---
+++ src/app/healthcare-stocks-news/page.tsx
@@ -0,0 +1,5 @@
+import { SpecialMarketNewsPage } from '@/components/special-news/SpecialMarketNewsPage';
+
+export default function HealthcareStocksNewsPage() {
+  return <SpecialMarketNewsPage topic="healthcare-stocks" />;
+}
