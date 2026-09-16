b21b9609f442e494331dbd470caaf8c654176edb
Mohammed alqallaf <elqallaf09@gmail.com>
2026-09-16 22:35:35 +0300

    feat(news): add mergers acquisitions news page

---
+++ src/app/mergers-acquisitions-news/page.tsx
@@ -0,0 +1,5 @@
+import { SpecialMarketNewsPage } from '@/components/special-news/SpecialMarketNewsPage';
+
+export default function MergersAcquisitionsNewsPage() {
+  return <SpecialMarketNewsPage topic="mergers-acquisitions-news" />;
+}
